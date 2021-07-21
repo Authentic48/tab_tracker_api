78a21c19869bc9b7d33eb33bcc6f895990e4c8c0
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-07-22 00:36:47 +0700

    protect middleware added

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const { registerUser, authUser, getUser } = require('../Controllers/authController')
+const { protect } = require('../Middlewares/authMiddleware')
 
 const router = express.Router()
 
 router.route('/register').post(registerUser)
 router.route('/login').post(authUser)
-router.route('/').get(getUser)
+router.route('/').get(protect, getUser)
 
 
 
