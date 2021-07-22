a209ff6f261656a372866194cd0174e8d58b2105
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-07-22 15:25:45 +0700

    update Song done

--- Controllers/songController.js
+++ Controllers/songController.js
@@ -30,5 +30,33 @@ const getAllSongs = asyncHandler(async (req, res) => {
     return res.json(songs)
 })
 
-module.exports = { addSong,  getAllSongs };
+// @desc    get all songs
+// @route   GET /api/songs
+// @Access  Public
+const updateSong = asyncHandler(async (req, res) => {
+    
+    const song = await Song.findByPk(req.params.id)
+
+    if(song)
+    {
+        song.title = req.body.title || song.title,
+        song.artist = req.body.artist || song.artist,
+        song.genre = req.body.genre || song.genre,
+        song.album = req.body.album || song.album,
+        song.albumImageUrl = req.body.albumImageUrl || song.albumImageUrl,
+        song.youtubeId = req.body.youtubeId || song.youtubeId,
+        song.lyrics = req.body.lyrics || song.lyrics,
+        song.tab = req.body.tab || song.tab
+
+        const updatedSong = await song.save()
+
+        return res.status(201).json(updatedSong)
+        
+    }else
+    {
+        return res.json('Song not found..')
+    }
+})
+
+module.exports = { addSong,  getAllSongs, updateSong };
 
--- Routes/songRoute.js
+++ Routes/songRoute.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const { addSong, getAllSongs } = require('../Controllers/songController')
+const { addSong, getAllSongs, updateSong } = require('../Controllers/songController')
 const { protect, admin, artist } = require('../Middlewares/authMiddleware')
 
 const router = express.Router()
 
 router.route('/create').post(addSong)
 router.route('/').get(getAllSongs)
+router.route('/:id').post(updateSong)
+
 
 module.exports = router;
 
