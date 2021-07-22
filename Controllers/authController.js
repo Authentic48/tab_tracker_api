706c0dd9d9d0e594caade069ecf676b4de744137
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-07-22 16:33:58 +0700

    User CRUD Done

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -11,7 +11,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const { name, email, password, isAdmin, isArtist } = req.body;
 
-    const userExist = await User.findOne({ where: { email: email } } )
+    const userExist = await User.findOne({ where: { email: email } })
 
     if (userExist) {
         res.status(400)
@@ -46,23 +46,23 @@ const registerUser = asyncHandler(async (req, res) => {
 const authUser = asyncHandler(async (req, res) => {
 
     const { email, password } = req.body;
-    const user =  await User.findOne({ where: { email: email } } )
+    const user = await User.findOne({ where: { email: email } })
 
     if (user) {
 
         const validPassword = await bcrypt.compare(password, user.password)
-        
-        if(validPassword)
-        {
+
+        if (validPassword) {
             return res.json({
                 id: user.id,
                 email: user.email,
                 name: user.name,
                 isAdmin: user.isAdmin,
+                isArtist: user.isArtist,
                 token: generateWebToken(user.id)
-    
+
             })
-        }else {
+        } else {
             return res.json('Invalid password')
         }
     } else {
@@ -75,11 +75,79 @@ const authUser = asyncHandler(async (req, res) => {
 // @desc   Get list of all users 
 // @route   GET /api/users
 // @Access  Private/admin
-const getUser =  asyncHandler(async (req, res) => { 
-      
+const getUsers = asyncHandler(async (req, res) => {
     const users = await User.findAll({})
     return res.json(users)
 })
 
-module.exports = { registerUser, authUser, getUser };
+// @desc   Update user 
+// @route   GET /api/users
+// @Access  Private/ protect / admin / artist
+const updateUser = asyncHandler(async (req, res) => {
+
+    const user = await User.findByPk(req.user.id)
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        user.isArtist = req.body.isArtist || user.isArtist
+        user.isAdmin = req.body.isAdmin || user.isAdmin
+
+        console.log(req.body.isArtist)
+
+        if (req.body.password) {
+            const hashedPassword = await bcrypt.hash(req.body.password, 10)
+            user.password = hashedPassword
+        }
+
+        const updatedUser = await user.save()
+        res.json({
+
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            isArtist: updatedUser.isArtist,
+            token: generateWebToken(updatedUser.id),
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+// @desc   Delete user 
+// @route   GET /api/users
+// @Access  Private/admin
+const deleteUser = asyncHandler(async (req, res) => {
+
+    const user = await User.findByPk(req.params.id)
+    if (user) {
+        await user.destroy()
+        res.json({ message: "user has Been deleted" })
+
+    } else {
+        res.status(404)
+        res.json({ message: "User Not Found" })
+    }
+
+})
+
+// @desc   get a single user
+// @route   GET /api/users
+// @Access  Private / protect admin 
+const getUserById = asyncHandler(async (req, res) => {
+
+    const user = await User.findByPk(req.params.id)
+    if (user) {
+        res.json(user)
+
+    } else {
+        res.status(404)
+        res.json({ message: "User Not Found" })
+    }
+
+})
+
+
+
+module.exports = { registerUser, authUser, getUsers, deleteUser, getUserById, updateUser };
 
--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,13 +1,22 @@
 const express = require('express')
-const { registerUser, authUser, getUser } = require('../Controllers/authController')
+const { registerUser, authUser, getUsers, getUserById, updateUser, deleteUser } = require('../Controllers/authController')
 const { protect, admin, artist } = require('../Middlewares/authMiddleware')
 
 const router = express.Router()
 
 router.route('/register').post(registerUser)
-router.route('/login').post(authUser)
-router.route('/').get(protect, admin, getUser)
 
+router.route('/login').post(authUser)
+
+router.route('/').get(protect, admin, getUserById)
+
+router.route('/update').put(protect, updateUser)
+
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
 module.exports = router;
 
