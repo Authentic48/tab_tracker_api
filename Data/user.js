2a21e17effafcd5f4522d60de6f34c17e3129b97
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-07-24 00:40:31 +0700

    data updated

--- Data/user.js
+++ Data/user.js
@@ -6,19 +6,19 @@ const users = [
     {
         name: process.env.User_1_name,
         email: process.env.User_1_email,
-        password: bcrypt.hashSync(process.env.User_1_password),
+        password: bcrypt.hashSync(process.env.User_PWD),
         isAdmin: true
     },
     {
         name: process.env.User_2_name,
         email: process.env.User_2_email,
-        password: bcrypt.hashSync(process.env.User_2_password),
+        password: bcrypt.hashSync(process.env.User_PWD),
         isArtist: true
     },
     {
         name: process.env.User_3_name,
         email: process.env.User_3_email,
-        password: bcrypt.hashSync(process.env.User_2_password),
+        password: bcrypt.hashSync(process.env.User_PWD),
     },
 ]
 
